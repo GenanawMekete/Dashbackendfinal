30ea96e8414560ad7d840daa22e3072144dd263a
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-15 16:46:45 +0300

    Change Telegram webhook route to '/'

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN);
 
-router.post('/telegram', (req, res) => {
+router.post('/', (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
